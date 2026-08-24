888ca00b7393b3b02c5486bc08a4c1f37bf73db8
Henrique B. Prado <168011450+HenriqueDeBritto@users.noreply.github.com>
2026-08-24 14:54:44 -0300

    Initial commit

---
+++ src/main/java/com/henrique/ImplantaHub/ImplantaHubApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.ImplantaHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplantaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplantaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique/ImplantaHub/ImplantaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.ImplantaHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplantaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
